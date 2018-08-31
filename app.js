e539ea035cc6616246923a2026c810d6440d3540
Pujan Bajracharya <pujanisme@gmail.com>
2018-08-31 16:29:08 +0545

    Created Component

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-        message: 'From Zero To Hero With Vue!',
-        hasMessage: true,
-        vue: 'Vue.js is an open source JavaScript Framework.',
-        selected: '',
-        messages: [{
-                text: 'Hello World'
-            },
-            {
-                text: 'Hello Nepal'
-            },
-            {
-                text: 'Hello Kathmandu'
-            },
+ Vue.component('navbar', {
+     template: `
+    <header>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+        </ul>
+    </header>
+    `
 
-        ]
-    },
-    methods: {
-        logMessage() {
-            console.log(this.message);
-        },
-        toggleMessage() {
-            this.hasMessage = !this.hasMessage;
-        }
-    }
-});
+ });
 
+ const app = new Vue({
+         el: '#app',
+
+         data: {
+             message: 'From Zero To Hero With Vue!',
+             hasMessage: true,
+             vue: 'Vue.js is an open source JavaScript Framework.',
+             selected: '',
+             messages: [{
+                     text: 'Hello World'
+                 },
+                 {
+                     text: 'Hello Nepal'
+                 },
+                 {
+                     text: 'Hello Kathmandu'
+                 },
+
+             ]
+         },
+         methods: {
+             logMessage() {
+                 console.log(this.message);
+             },
+             toggleMessage() {
+                 this.hasMessage = !this.hasMessage;
+             }
+         }
+
+
+
+     },
+
+ );
